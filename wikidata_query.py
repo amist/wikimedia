db503a568510774864e7661cf39066c132ea0bd2
Amitay Stern <amitay.stern@cs-robotics.com>
2019-03-26 20:50:53 +0200

    Use pandas

--- wikidata_query.py
+++ wikidata_query.py
@@ -3,10 +3,10 @@ import json
 import hashlib
 import requests
 import urllib
-import datetime
 import matplotlib.pyplot as plt
+import pandas as pd
 
-class WikimediaFetcher(object):
+class WikidataFetcher(object):
     def __init__(self):
         self.queries_cache_dir = 'queries_cache'
         if not os.path.exists(self.queries_cache_dir):
@@ -21,22 +21,29 @@ class WikimediaFetcher(object):
         query = urllib.parse.quote(query)
         cmd = 'https://query.wikidata.org/sparql?format=json&query={}'.format(query)
         filename = os.path.join(self.queries_cache_dir, '{}.json'.format(self.hash_string(cmd)))
+        
         if os.path.exists(filename):
             with open(filename, 'r') as data_file:
                 return json.load(data_file)
+
         results = requests.get(cmd).json()
         with open(filename, 'w') as data_file:
             json.dump(results, data_file)
         return results
 
 
+    def dataframe_query(self, query):
+        results = self.run_query(query)
+        columns = results['head']['vars']
+        rows = [[res.get(col) and res[col]['value'] for col in columns] for res in results['results']['bindings']]
+        d = [{col: res.get(col) and res[col]['value'] for col in columns} for res in results['results']['bindings']]
+        df = pd.DataFrame(d)
+        return df
+
+
 class PresidentsStats(object):
     def __init__(self):
-        pass
-
-
-    def get_dates_data(self):
-        q = '''
+        self.query = '''
 SELECT ?presLabel ?pres ?start_date ?end_date ?birth_date ?death_date WHERE {
   ?pres wdt:P31 wd:Q5 .
   ?pres p:P39 ?presidency_statement .
@@ -50,46 +57,38 @@ SELECT ?presLabel ?pres ?start_date ?end_date ?birth_date ?death_date WHERE {
 }
 ORDER BY ?start_date
 '''
-    
-        wf = WikimediaFetcher()
-        results = wf.run_query(q)
-        return results
 
 
-    def create_timeline(self):
-        '''timeline of how many living presidents or ex-presidents there are.'''
-        timeline = []
-        results = self.get_dates_data()
-        for r in results['results']['bindings']:
-            #print(r)
-            start_date = r['start_date']['value']
-            timeline.append([start_date, 1])
-            if 'death_date' in r:
-                death_date = r['death_date']['value']
-                timeline.append([death_date, -1])
-        timeline.sort(key=lambda x: x[0])
-        cum = 0
-        c_timeline = []
-        for t in timeline:
-            #print(t)
-            moment = datetime.datetime.strptime(t[0], '%Y-%m-%dT%H:%M:%SZ')
-            c_timeline.append([moment - datetime.timedelta(seconds=1), cum])
-            cum += t[1]
-            c_timeline.append([moment, cum])
-            #c_timeline.append([t[0], cum])
-            #print('--->', c_timeline[-1])
-        return c_timeline
+    def get_dataframe(self):
+        wf = WikidataFetcher()
+        df = wf.dataframe_query(self.query)
+
+        df = pd.melt(df, id_vars=['pres', 'presLabel'], var_name='event', value_name='date')
+        df = df.sort_values(by=['date'])
+        return df
+
+
+    def event_to_num(self, row):
+        if row['event'] in ['birth_date', 'end_date']:
+            return 0
+        if row['event'] == 'start_date':
+            return 1
+        if row['event'] == 'death_date':
+            return -1
+
+
+    def get_dates_data(self):
+        df = ps.get_dataframe()
+        df = df[['presLabel', 'date', 'event']]
+        df = df[df['date'].notnull()]
+        df['change'] = df.apply(self.event_to_num, axis=1)
+        df['total_number'] = df['change'].cumsum(axis=0)
+        return df
 
 
     def plot_graph(self):
-        timeline = self.create_timeline()
-        #print(timeline)
-        #x = [datetime.datetime.strptime(s[0], '%Y-%m-%dT%H:%M:%SZ') for s in timeline]
-        x = [s[0] for s in timeline]
-        y = [s[1] for s in timeline]
-        #print(x)
-        #print(y)
-        plt.plot(x, y)
+        df = ps.get_dates_data()
+        df.plot(x='date', y='total_number', drawstyle='steps', legend=False)
         plt.show()
 
 
